39efcdf0e412155401261a600fb0138df8da57e9
Grey <81944598+Grey03@users.noreply.github.com>
2022-03-27 17:37:00 -0400

    Add files via upload

---
+++ imgttalgo.py
@@ -0,0 +1,111 @@
+#http://xahlee.info/comp/unicode_braille.html
+#https://home.unicode.org/
+#https://www.tutorialspoint.com/working-with-images-in-python
+#https://www.geeksforgeeks.org/python-pil-getpixel-method/
+
+import math
+import os
+import os.path
+
+from PIL import Image
+
+    #Needs to take in the file name, dimensions if needed, invert y/n, contrast
+    #then does the old algorithm 
+    #then opens the txt for you
+
+def ImgToTxt(info):
+    filename = (info["filename"])
+    savelocation = (info["savelocation"])
+    changeDimensions = (info["changeDimensions"])
+    finalX = (info["FinalX"])
+    finalY = (info["FinalY"])
+    global Invert
+    Invert = (info["Invert"])
+    global ContrastSetting
+    ContrastSetting = (info["Contrast"])
+
+    try:
+        image = Image.open(filename).convert("L")
+    except:
+        print ("Invalid File-type")
+
+    #-----FUNCTIONS-----
+
+    #Img Resize
+    def correctSize(img):
+        while img.size[0] * img.size[1] > 3000000:
+            img = img.resize((math.ceil(img.size[0]/2),math.ceil(img.size[1]/2)))
+        while (img.size[0]%2 != 0):
+            img = img.resize((img.size[0] + 1, img.size[1]))
+        while (img.size[1]%3 != 0):
+            img = img.resize((img.size[0], img.size[1] + 1))
+
+        return (img)
+
+    def chunkAnalyzer(chunklist):
+        global ContrastSetting
+        global Invert 
+        thelist = [
+        chunklist[5],
+        chunklist[3],
+        chunklist[1],
+        chunklist[4],
+        chunklist[2],
+        chunklist[0]
+      ]
+        temp = ("")
+        for n in range(len(thelist)):
+            if thelist[n] >= ContrastSetting:
+                thelist[n] = 1 - Invert
+            else:
+                thelist[n] = 0 + Invert
+        for i in range(len(thelist)):
+            temp = (str(temp) + str(thelist[i]))
+        temp = int(temp,2)
+        temp = (temp + 10240)
+        return (chr(temp))
+
+
+    #Chunk Maker
+    def mainAlgo(img):  
+        temp = []
+        temp2 = []
+        for y in range(0,img.size[1],3):
+            temp.append(temp2)
+            temp2 = []
+            for x in range(0,img.size[0],2):
+                chunk = [
+                (img.getpixel((x,y))),
+                (img.getpixel((x + 1,y))),
+                (img.getpixel((x,y + 1))),
+                (img.getpixel((x + 1,y + 1))),
+                (img.getpixel((x,y + 2))),
+                (img.getpixel((x + 1,y + 2)))
+                ]
+                temp2.append(chunkAnalyzer(chunk))
+        return (temp)
+
+    #-----RUN-----
+
+    #Resize img
+    image = correctSize(image) 
+    if (finalX != None and finalY != None and int(changeDimensions) == 1):
+        print ("RESIZE")
+        image = image.resize((int(finalX) * 2, int(finalY) * 3))
+
+        
+        
+
+    final = mainAlgo(image)
+
+    final.pop(0)
+
+    #final = (["test","test"],["test","test"])
+
+    with open(str(savelocation) + "/output.txt", "w", encoding='utf-8') as f:
+      for y in range(len(final)):
+        if y != 0:
+          f.write("\n")
+        for x in range(len(final[0])):
+            f.write((final[y][x]))
+    os.startfile(str(savelocation) + "/output.txt")
---
+++ main.py
@@ -0,0 +1,127 @@
+﻿try:
+    from tkinter import *
+    import tkinter as tk
+    from tkinter import filedialog
+except:
+    crashed = input("tkinter could not import")
+
+import imgttalgo
+
+global FinX
+FinX = 0
+global FinY
+FinY = 0
+global filename
+global saveLoc
+
+def browseFiles():
+    global filename
+    filename = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("PNGs",
+                                                        "*.png*"),
+                                                       ("JPGs",
+                                                        "*.jpg*")))
+
+    label_file_explorer.configure(text="Selected File: " + filename)
+
+def saveLocation():
+    global saveLoc
+    saveLoc = filedialog.askdirectory()
+    label_save_explorer.configure(text = "Selected File: " + saveLoc)
+     
+#actual stuff start here
+root = Tk()
+root.title("Image To Text Generator V2.0")
+
+
+
+#-----Buttns n stuffs----
+
+#File Stuff
+label_file_explorer = Label(root, text = "Choose Image *")
+button_explore = Button(root, text = "Browse Files", command = browseFiles)
+
+label_save_explorer = Label(root, text = "Choose Save Location *")
+button_saveLoc = Button(root, text = "Save Location", command = saveLocation)
+
+#Change Dimension Stuff
+global ChangeDi
+changeDi = IntVar()
+changeDiBox = Checkbutton(root, text="Change Dimensions", variable=changeDi)
+DimXLabel = Label(root, text = "Enter Desired X")
+global DimensionX
+DimensionX = Entry(root, text="X")
+DimYLabel = Label(root, text = "Enter Desired Y")
+global DimensionY
+DimensionY = Entry(root, text="Y")
+
+#Invert
+global Invert
+Invert = IntVar()
+InvertBox = Checkbutton(root, text="Invert text", variable=Invert)
+
+#Contrast
+contrastLabel = Label(root,text="Contrast Level")
+global contrast
+contrast = Scale(root, from_=1, to=255, orient=HORIZONTAL)
+contrast.set(128)
+
+#Generate Button
+#ADDDDDDDDDDDD THE GENERATE FUNCTION HEREERERE!!!
+def sendconvert():
+    global filename
+    global saveLoc
+    global changeDi
+    global FinX
+    global FinY
+    global Invert
+    global contrast
+
+
+    if (filename == None):
+        browseFiles()
+    if (saveLoc == None):
+        saveLocation()
+
+    if (changeDi == 1 and FinX == None or FinY == None):
+        print ("No X or Y for Changed Dimension")
+        changeDi = 0
+    else:
+
+        Default = {
+        "filename": filename,
+        "savelocation": saveLoc,
+        "changeDimensions": changeDi.get(),
+        "FinalX": DimensionX.get(),
+        "FinalY": DimensionY.get(),
+        "Invert": Invert.get(),
+        "Contrast": contrast.get()
+        }
+        imgttalgo.ImgToTxt(Default)
+
+Generate = Button(root, text="Generate Text", command=sendconvert)
+
+#----Positions!----
+label_file_explorer.grid(row = 0, column = 0)
+button_explore.grid(row = 1, column = 0)
+
+label_save_explorer.grid(row = 0, column = 1)
+button_saveLoc.grid(row = 1, column = 1)
+
+
+changeDiBox.grid(row = 2, column = 0)
+
+DimXLabel.grid(row = 3, column = 0)
+DimYLabel.grid(row = 3, column = 1)
+DimensionX.grid(row = 4, column = 0)
+DimensionY.grid(row = 4, column = 1)
+
+InvertBox.grid(row=5, column = 0)
+
+contrastLabel.grid(row=6, column = 0)
+contrast.grid(row=7, column = 0)
+
+Generate.grid(row=8, column = 0)
+
+root.mainloop()
